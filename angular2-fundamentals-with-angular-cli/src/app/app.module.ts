834f601c0ae41f8403844862786a1d253c9a670c
Jose Diaz <jose.diaz@joedayz.pe>
2017-03-18 17:37:25 -0500

    Binding bidireccional

--- angular2-fundamentals-with-angular-cli/src/app/app.module.ts
+++ angular2-fundamentals-with-angular-cli/src/app/app.module.ts
@@ -1,15 +1,20 @@
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
-import {AppComponent} from "./app.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    FormsModule,
+    CommonModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ],
+  declarations: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
