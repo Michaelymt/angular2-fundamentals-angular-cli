f1f1d3ca490eafdd07d68cbd91623c438ed5705c
Jose Diaz <jose.diaz@joedayz.pe>
2017-03-18 18:04:55 -0500

    ng for

--- angular2-fundamentals-with-angular-cli/src/app/app.component.ts
+++ angular2-fundamentals-with-angular-cli/src/app/app.component.ts
@@ -1,31 +1,53 @@
 import { Component } from '@angular/core';
 
+interface Pasajero {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <input 
-        type="text" 
-        [value]="name" 
-        (input)="handleChange($event.target.value)">
-      
-      <template [ngIf]="name.length > 2">
-        <div>
-          Buscando por... {{ name }}
-        </div>
-      </template>
-
-      <div *ngIf="name.length > 2">
-        Buscando por... {{ name }}
-      </div>
-
+      <h3>Lista de Pasajeros</h3>
+      <ul>
+        <template ngFor let-pasajero let-i="index" [ngForOf]="pasajeros">
+          <li>
+            {{ i }}: {{ pasajero.fullname }}
+          </li>
+        </template>
+      </ul>
+      <h3>Lista de pasajeros (versión más utilizada)</h3>
+      <ul>
+        <li *ngFor="let pasajero of pasajeros; let i = index;">
+          {{ i }}: {{ pasajero.fullname }}
+        </li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
-  name: string = '';
-  handleChange(value: string) {
-    this.name = value;
-  }
+  pasajeros: Pasajero[] = [{
+    id: 1,
+    fullname: 'Elias',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Felipe',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'Deborah',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Maria',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Daniel',
+    checkedIn: false
+  }];
 }
