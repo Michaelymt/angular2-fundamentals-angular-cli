2feaef9861e8feccdbe982c6fdf72a7f593af2bf
Jose Diaz <jose.diaz@joedayz.pe>
2017-03-18 16:58:32 -0500

    Binding de propiedades

--- angular2-fundamentals-with-angular-cli/src/app/app.component.ts
+++ angular2-fundamentals-with-angular-cli/src/app/app.component.ts
@@ -5,22 +5,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title + '!' }}
-      <div>
-        {{ numberOne + numberTwo }}
-      </div>
-      <div>
-        {{ isHappy ? ':)' : ':(' }}
-      </div>
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{ title }}</h1>
+      <img [src]="logo">
+      <input type="text" [value]="name">
+      <div>{{ name }}</div>
     </div>
   `
 })
 export class AppComponent {
   title: string;
-  isHappy: boolean = false;
-  numberOne: number = 1;
-  numberTwo: number = 2;
+  name: string = 'Oswaldo';
+  logo: string = '/assets/img/logo.gif';
   constructor() {
-    this.title = 'Usando Interpolacion';
+    this.title = 'Binding de propiedades';
   }
 }
